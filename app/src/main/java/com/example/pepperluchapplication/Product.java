96d5c1a8feb38dd7166704fc033461895e811f8a
MinhTonne <phmtoan2005@gmail.com>
2022-05-07 22:20:31 +0700

    complete home page

---
+++ app/src/main/java/com/example/pepperluchapplication/Product.java
@@ -0,0 +1,28 @@
+package com.example.pepperluchapplication;
+
+public class Product {
+    int image;
+    private String name;
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Product(int image, String name) {
+        this.image = image;
+        this.name = name;
+    }
+
+}
